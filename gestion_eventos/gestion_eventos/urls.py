2dfa0a69220ad21e5e284919641e47d481a6aae2
Ortega Hamel <ortega.hamel@gmail.com>
2025-10-08 00:13:30 -0300

    Subiendo templates

--- gestion_eventos/gestion_eventos/urls.py
+++ gestion_eventos/gestion_eventos/urls.py
@@ -1,10 +1,16 @@
-
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from eventos_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('', views.HomeView.as_view(), name='home'),
+    path('event/<int:pk>/', views.EventDetailView.as_view(), name='event_detail'),
+    path('event/new/', views.EventCreateView.as_view(), name='event_create'),
+    path('event/<int:pk>/edit/', views.EventUpdateView.as_view(), name='event_edit'),
+    path('event/<int:pk>/delete/', views.EventDeleteView.as_view(), name='event_delete'),
+    path('event/<int:pk>/attend/', views.attend_event, name='attend_event'),
 ]
